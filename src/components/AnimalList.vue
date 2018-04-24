c15cbf9ad1e64a25ecbd41a48b8ea8ec86f4940c
NoHeal4u <miodragnikolic@yahoo.com>
2018-04-24 19:09:03 +0200

    drugi

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -11,7 +11,8 @@
     <tr v-for= "(animal , key) in animals" :key = "key">
       <td>{{ animal.specie }}</td>
       <td>{{ animal.name }}</td>
-      <td>{{ animal.dateOfBirth }}</td>
+      <td v-if = "animal.dateOfBirth != null">{{ animal.dateOfBirth }}</td>
+      <td v-else >Unknown</td>
     </tr>
       
     </table>
@@ -29,11 +30,11 @@ export default {
 
     return {
       animals: [
-      { specie: 'Konj' , name: 'Kole' , dateOfBirth: '2015' },
-      { specie: 'Krava' , name: 'Belka' , dateOfBirth: '2016' },
-      { specie: 'Svinja' , name: 'Gile' , dateOfBirth: '2018' },
-      { specie: 'Koza' , name: 'Angora' , dateOfBirth:  '2017 '},
-      { specie: 'Dzudzabajac' , name: 'Malaksijandar' , dateOfBirth: '956' }
+      { specie: 'Konj' , name: 'Kole' , dateOfBirth: 2015 },
+      { specie: 'Krava' , name: 'Belka' , dateOfBirth: 2016 },
+      { specie: 'Svinja' , name: 'Gile' , dateOfBirth: 2018 },
+      { specie: 'Koza' , name: 'Angora' , dateOfBirth:  2017 },
+      { specie: 'Dzudzabajac' , name: 'Malaksijandar' , dateOfBirth: null  }
 
       ]
     }

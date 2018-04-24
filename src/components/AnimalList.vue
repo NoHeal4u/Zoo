9b5ffd3eb46f97e9b89d31db87ada68703e1ebf3
NoHeal4u <miodragnikolic@yahoo.com>
2018-04-24 21:31:25 +0200

    uradjeno sve

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,11 +1,39 @@
 <template>
 
   <div>
-    <table style="width:80%" border="1px">
+
+    <form @submit.prevent="addAnimal">
+
+
+
+  <label>Specie</label>
+  <input v-model = "newAnimal.specie" type = "text"/>
+
+  <label>Name</label>
+  <input  v-model = "newAnimal.name" type = "text"/>
+
+  <label>Date of birth</label>
+  <input  v-model = "newAnimal.dateOfBirth" type = "number"/>
+
+  <label>Sector</label>
+  <input type="radio" v-model ="newAnimal.sector"  :value = "sectors[0]" checked> Wild animal<br>
+  <input type="radio" v-model ="newAnimal.sector"  :value = "sectors[1]" > Domestic animal <br>
+
+  <button type = "submit">Add Animal</button>
+  
+  <!-- {{newAnimal}} -->
+
+  </form>
+
+
+
+
+    <table style="width:50%" border="1px">
     <tr>
       <th>Species</th>
       <th>Name</th>
       <th>Year of birth</th>
+      <th>Sector</th>
 
 
     </tr>
@@ -15,16 +43,49 @@
       <td>{{ animal.name }}</td>
       <td v-if = "animal.dateOfBirth != null">{{ animal.dateOfBirth }}</td>
       <td v-else >Unknown</td>
+      <td>{{animal.sector.name}}</td>
       <td><button @click = "removeAnimal(animal)">Remove</button></td>
       <td><button @click = "moveToTop(animal)">Move to top</button></td>
     </tr>
+
       
+    </table >
+
+
+    <table style="width:50%" border="1px" >
+
+      <tr>
+      <th>Sector</th>
+      </tr>
+
+      <tr>
+
+        <td>{{sectors[0].name}}</td>
+        <td><button @click = "viewAnimals(sectors[0])">View all wild animals</button></td>
+
+      </tr>
+
+      <tr>
+        <td>{{sectors[1].name}}</td>
+        <td><button @click = "viewAnimals(sectors[1])" >View all domestic animals</button></td>
+      </tr>
     </table>
+
+
   </div>
 
 </template>
 
 <script>
+
+
+const sectors = [
+
+      {name: 'Wild'},
+      {name: 'Domestic'}
+
+
+      ];
 
 export default {
 
@@ -33,14 +94,32 @@ export default {
   data() {
 
     return {
+      sectors:sectors,
       animals: [
-      { specie: 'Konj' , name: 'Kole' , dateOfBirth: 2015 },
-      { specie: 'Krava' , name: 'Belka' , dateOfBirth: 2016 },
-      { specie: 'Svinja' , name: 'Gile' , dateOfBirth: 2018 },
-      { specie: 'Koza' , name: 'Angora' , dateOfBirth:  2017 },
-      { specie: 'Dzudzabajac' , name: 'Malaksijandar' , dateOfBirth: null  }
+      { specie: 'Konj' , name: 'Kole' , dateOfBirth: 2015 , sector: sectors[1] },
+      { specie: 'Krava' , name: 'Belka' , dateOfBirth: 2016, sector: sectors[1] },
+      { specie: 'Svinja' , name: 'Gile' , dateOfBirth: 2018, sector: sectors[1] },
+      { specie: 'Koza' , name: 'Angora' , dateOfBirth:  2017, sector: sectors[1] },
+      { specie: 'Dzudzabajac' , name: 'Malaksijandar' , dateOfBirth: null, sector: sectors[0]  }
 
-      ]
+      ],
+
+      newAnimal: {
+        specie: '',
+        name: '',
+        yearOfBirth: '',
+        sector: ''
+      },
+
+
+      
+
+
+
+
+
+
+
     }
   },
 
@@ -58,6 +137,16 @@ export default {
       this.animals.splice(this.animals.indexOf(animal),1);
       this.animals.unshift(animal);
 
+    },
+
+    addAnimal() {
+    this.animals.push(this.newAnimal)
+    },
+
+    viewAnimals(sectors){
+      this.animals.forEach(function(animal) {
+      if (sectors.name === animal.sector.name) { window.alert(animal.name)}
+      });
     }
   }
 }
